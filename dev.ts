3557cc50e0114bb2b771900da3f36da876b003d0
Renato Vicente <revi66@hotmail.com>
2020-09-02 17:10:13 -0300

    chore: add dev script

---
+++ dev.ts
@@ -0,0 +1,21 @@
+import { handler } from './src/index'
+
+async function run (): Promise<void> {
+  const body = JSON.stringify({
+    test: 'testing'
+  })
+  const event = {
+    body
+  }
+  try {
+    const result = await handler(event, null, null)
+    return result
+  } catch (error) {
+    return error
+  }
+}
+
+run().then(
+  (result) => { console.log(result) },
+  (result) => { console.log(result) }
+)
